13cddc68164086c9199119f60e6f0836e6991b21
Derrick Hinkle <vorpalhex@users.noreply.github.com>
2016-11-29 21:25:54 -0600

    Git completion

--- setup.sh
+++ setup.sh
@@ -7,9 +7,12 @@ ln -sF $cwd/vim/.vimrc ~/.vimrc
 
 cp ~/.bashrc ~/.bashrc_old
 ln -sF $cwd/bash/.bashrc ~/.bashrc
+ln -sF $cwd/bash/git-completion.bash ~/.git-completion.bash
 chmod u+x ~/.bashrc
 
 ln -sF $cwd/git/.gitconfig ~/.gitconfig
 ln -sF $cwd/git/.gitignore ~/.gitignore
 
 apm install dracula-theme
+
+source ~/.bashrc
